574984fd104590497dcac1a3eb7abf4a2597eb35
markymelnik <markemelnik@gmail.com>
2022-12-08 10:34:39 -0500

    Testing the inclusion of a .css file

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,16 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'production'
+  mode: 'production',
+  module: { 
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
